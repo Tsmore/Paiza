94396aad7f23dafe35a3eef34ac9e9212e8e0ded
Tsmore <143774064+Tsmore@users.noreply.github.com>
2024-01-20 18:07:52 +0900

    completed

---
+++ rank-D/cloth-price.rb
@@ -0,0 +1,8 @@
+# 服の値段
+
+a = gets.chomp.to_i
+b = gets.chomp.to_i
+
+total = a + b
+
+puts total
---
+++ rank-D/summer-homework.rb
@@ -0,0 +1,8 @@
+# 夏休みの宿題
+
+a = gets.chomp.to_i
+b = gets.chomp.to_i
+
+pages_left = 500 - (a + b)
+
+puts pages_left

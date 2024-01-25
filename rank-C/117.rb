50f2bae10e4c9b2a304337b9c68b3689fb6e42f6
Tsmore <143774064+Tsmore@users.noreply.github.com>
2024-01-26 00:18:09 +0900

    計算結果がずれるけどわからない...

---
+++ rank-C/117.rb
@@ -0,0 +1,17 @@
+# 大量出店
+
+def calc_unprofitable_stores(n, m, a, b, c, sales)
+  unprofitable_stores = 0
+  sales.each do |sold|
+    total_profit = sold * c * m
+    total_cost = a + (b * m)
+    unprofitable_stores += 1 if total_profit < total_cost
+  end
+  unprofitable_stores
+end
+
+n, m = gets.split.map(&:to_i)
+a, b, c = gets.split.map(&:to_i)
+sales = n.times.map { gets.to_i }
+
+puts calc_unprofitable_stores(n, m, a, b, c, sales)
